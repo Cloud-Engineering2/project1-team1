1ca4fb7ec0a5f858e3f44336afc8ea48c65e891a
sungchilll <csk2751@gmail.com>
2024-12-10 16:51:57 +0900

    :tada: init : create project

---
+++ src/main/java/com/ce/myallstarteam/MyAllStarTeamApplication.java
@@ -0,0 +1,13 @@
+package com.ce.myallstarteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAllStarTeamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAllStarTeamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ce/myallstarteam/MyAllStarTeamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ce.myallstarteam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAllStarTeamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
