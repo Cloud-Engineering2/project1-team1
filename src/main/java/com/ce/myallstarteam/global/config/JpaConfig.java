8a84c405363e89105fa45a1b5bc55933484415fe
amberlee0201 <amber.lee.020100@gmail.com>
2024-12-13 13:23:28 +0900

    :pencil2: fix: enable jpa auditing

---
+++ src/main/java/com/ce/myallstarteam/global/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.ce.myallstarteam.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+}
